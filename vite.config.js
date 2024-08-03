9ef7ce24392bb9c07414a0ced4b15665a4c9c0ac
Anonymous-025 <unknownjesvin>
2024-08-03 17:13:32 +0530

    edited vite.config

--- vite.config.js
+++ vite.config.js
@@ -3,7 +3,6 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 export default defineConfig({
-  base: '/MelodyCraft/', // Make sure this matches your repository name
+  // base: '/MelodyCraft/', // Make sure this matches your repository name
   plugins: [react()],
-  homepage: 'https://Anonymous-025.github.io/MelodyCraft/',
 })
